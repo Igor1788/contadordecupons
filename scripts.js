43b0ebcdae60f4517ae8b34b992d5cb3b3e55e8d
Igor <igor1788@gmail.com>
2023-11-03 18:19:42 -0300

    Second commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Configuração do Firebase (substitua pelos valores reais do seu projeto)
+// Configuração do Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyCp0DnKfWTCfx5zhsia_ejxcxrqdTCCmHM",
     authDomain: "primoflix-8691b.firebaseapp.com",
@@ -14,34 +14,53 @@ const firebaseConfig = {
   const database = firebase.database();
   
   const numeroRef = database.ref('UltimoNumero');
-
+  const chamadasRef = database.ref('Chamadas'); // Adicionado para buscar as chamadas
+  
   let valorAtual = null;
   let intervalo = null;
+  let chamadas = []; // Vai armazenar as chamadas
   
-  numeroRef.on('value', function(snapshot) {
-    const novoValor = snapshot.val();
-    if (valorAtual === null) {
-      valorAtual = novoValor; // Defina o valor inicial se for a primeira vez
-      atualizarContador(valorAtual);
-    } else {
-      if (intervalo) {
-        clearInterval(intervalo); // Limpe o intervalo existente se estiver contando
-      }
-      intervalo = setInterval(() => {
-        if (valorAtual < novoValor) {
-          valorAtual++;
-          atualizarContador(valorAtual);
-        } else if (valorAtual > novoValor) {
-          valorAtual--;
-          atualizarContador(valorAtual);
-        } else {
-          clearInterval(intervalo); // Limpe o intervalo quando chegar ao novo valor
-        }
-      }, 500); // Atualiza a cada meio segundo
-    }
-  });
-  
+  // Função para atualizar o contador
   function atualizarContador(valor) {
     document.getElementById('valorContador').innerText = valor;
   }
+  
+  // Função para atualizar a chamada aleatoriamente
+  function atualizarChamada() {
+    if (chamadas.length > 0) {
+      const indiceAleatorio = Math.floor(Math.random() * chamadas.length);
+      const chamadaSelecionada = chamadas[indiceAleatorio];
+      document.getElementById('chamadaTitulo').innerText = chamadaSelecionada;
+    }
+  }
+  
+  // Escutar as mudanças no campo UltimoNumero no Firebase
+  numeroRef.on('value', function(snapshot) {
+    const novoValor = snapshot.val();
+    if (valorAtual === null) {
+      valorAtual = novoValor;
+      atualizarContador(valorAtual);
+    } else {
+      if (intervalo) clearInterval(intervalo);
+      intervalo = setInterval(() => {
+        if (valorAtual < novoValor) {
+          valorAtual++;
+        } else if (valorAtual > novoValor) {
+          valorAtual--;
+        } else {
+          clearInterval(intervalo);
+        }
+        atualizarContador(valorAtual);
+      }, 500);
+    }
+  });
+  
+  // Escutar as mudanças no campo Chamadas no Firebase
+  chamadasRef.on('value', function(snapshot) {
+    chamadas = snapshot.val();
+    atualizarChamada(); // Atualize imediatamente com uma nova chamada
+  });
+  
+  // Atualizar a chamada a cada 10 segundos
+  setInterval(atualizarChamada, 10000);
   
