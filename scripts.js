0277e17bc464df022f00cbcc4e901e40ac4e4d98
Igor Gonçalves <igor1788@gmail.com>
2023-11-11 08:24:45 -0300

    Correcao de erro

--- scripts.js
+++ scripts.js
@@ -1,66 +1,65 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCp0DnKfWTCfx5zhsia_ejxcxrqdTCCmHM",
-    authDomain: "primoflix-8691b.firebaseapp.com",
-    databaseURL: "https://primoflix-8691b.firebaseio.com",
-    projectId: "primoflix-8691b",
-    storageBucket: "primoflix-8691b.appspot.com",
-    messagingSenderId: "466298232490",
-    appId: "1:466298232490:web:b7667bb30d4653fc943ad3"
-  };
-  
-  const app = firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
-  
-  const numeroRef = database.ref('UltimoNumero');
-  const chamadasRef = database.ref('Chamadas'); 
-  
-  let valorAtual = null;
-  let intervalo = null;
-  let chamadas = []; 
-  
-  function atualizarContador(valor) {
-    document.getElementById('valorContador').innerText = valor;
-    const percentual = (valor - 1) / (200 - 1); 
-    document.getElementById('barraProgresso').style.width = percentual * 100 + '%'; 
-  }
-  
+  apiKey: "AIzaSyCp0DnKfWTCfx5zhsia_ejxcxrqdTCCmHM",
+  authDomain: "primoflix-8691b.firebaseapp.com",
+  databaseURL: "https://primoflix-8691b.firebaseio.com",
+  projectId: "primoflix-8691b",
+  storageBucket: "primoflix-8691b.appspot.com",
+  messagingSenderId: "466298232490",
+  appId: "1:466298232490:web:b7667bb30d4653fc943ad3"
+};
 
-  function atualizarChamada() {
-    if (chamadas.length > 0) {
-      const indiceAleatorio = Math.floor(Math.random() * chamadas.length);
-      const chamadaSelecionada = chamadas[indiceAleatorio];
-      document.getElementById('chamadaTitulo').innerText = chamadaSelecionada;
-    }
-  }
-  
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
-  numeroRef.on('value', function(snapshot) {
-    const novoValor = snapshot.val();
-    if (valorAtual === null) {
-      valorAtual = novoValor;
+const numeroRef = database.ref('Numero');
+const chamadasRef = database.ref('Chamadas'); 
+
+let valorAtual = null;
+let intervalo = null;
+let chamadas = []; 
+
+function atualizarContador(valor) {
+  document.getElementById('valorContador').innerText = valor;
+  const percentual = (valor - 1) / (200 - 1); 
+  document.getElementById('barraProgresso').style.width = percentual * 100 + '%'; 
+}
+
+
+function atualizarChamada() {
+  if (chamadas.length > 0) {
+    const indiceAleatorio = Math.floor(Math.random() * chamadas.length);
+    const chamadaSelecionada = chamadas[indiceAleatorio];
+    document.getElementById('chamadaTitulo').innerText = chamadaSelecionada;
+  }
+}
+
+
+numeroRef.on('value', function(snapshot) {
+  const novoValor = snapshot.val();
+  if (valorAtual === null) {
+    valorAtual = novoValor;
+    atualizarContador(valorAtual);
+  } else {
+    if (intervalo) clearInterval(intervalo);
+    intervalo = setInterval(() => {
+      if (valorAtual < novoValor) {
+        valorAtual++;
+      } else if (valorAtual > novoValor) {
+        valorAtual--;
+      } else {
+        clearInterval(intervalo);
+      }
       atualizarContador(valorAtual);
-    } else {
-      if (intervalo) clearInterval(intervalo);
-      intervalo = setInterval(() => {
-        if (valorAtual < novoValor) {
-          valorAtual++;
-        } else if (valorAtual > novoValor) {
-          valorAtual--;
-        } else {
-          clearInterval(intervalo);
-        }
-        atualizarContador(valorAtual);
-      }, 500);
-    }
-  });
-  
+    }, 500);
+  }
+});
 
-  chamadasRef.on('value', function(snapshot) {
-    chamadas = snapshot.val();
-    atualizarChamada(); 
-  });
 
-  
+chamadasRef.on('value', function(snapshot) {
+  chamadas = snapshot.val();
+  atualizarChamada(); 
+});
 
-  setInterval(atualizarChamada, 10000);
-  
+
+
+setInterval(atualizarChamada, 10000);
